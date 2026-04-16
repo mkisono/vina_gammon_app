5dfe4686d4a4dda545fa180657fbc6775aa320a5
Masahiro Kisono <mkisono@gmail.com>
2026-04-16 19:03:42 +0900

    feat: add internationalization support with Japanese language and vocabulary settings

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,13 @@ import App from "./App.tsx";
 import "./index.css";
 import "@aws-amplify/ui-react/styles.css";
 import { Amplify } from "aws-amplify";
+import { I18n } from "aws-amplify/utils";
+import { translations } from "@aws-amplify/ui-react";
 import outputs from "../amplify_outputs.json";
 
 Amplify.configure(outputs);
+I18n.putVocabularies(translations);
+I18n.setLanguage("ja");
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
